4acb0d9ba8ade8b3d48e9b44bde652238f290d4f
vktorrykaliuk <viktorrykalyuk98@gmail.com>
2024-01-15 11:21:48 +0200

    add test for convert rates form object to list of objects

--- app/src/androidTest/java/com/example/currencyexchangertesttask/api/CurrencyConverterTests.kt
+++ app/src/androidTest/java/com/example/currencyexchangertesttask/api/CurrencyConverterTests.kt
@@ -2,6 +2,8 @@ package com.example.currencyexchangertesttask.api
 
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.example.currencyexchangertesttask.data.api.ApiService
+import com.example.currencyexchangertesttask.data.responce.Rates
+import com.example.currencyexchangertesttask.ui.models.CurrencyRates
 import com.example.currencyexchangertesttask.utils.Constants
 import junit.framework.TestCase
 import kotlinx.coroutines.runBlocking
@@ -9,9 +11,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.reflect.KMutableProperty
 
 @RunWith(AndroidJUnit4ClassRunner::class)
-class CurrencyConverterApiTest : TestCase() {
+class CurrencyConverterTests : TestCase() {
 
     @Test
     fun getExchangeRatesFromApi() {
@@ -22,7 +25,27 @@ class CurrencyConverterApiTest : TestCase() {
         assertEquals(test.isSuccessful, true)
     }
 
-
+    fun gatRates(): Rates {
+        val api = provideApi()
+        val test = runBlocking {
+            api.getExchangeRates()
+        }
+        return test.body()!!.rates
+    }
+    @Test
+    fun convertCurrencyRatesToList() {
+        val rates = gatRates()
+        val properties = rates::class.members
+        val list = mutableListOf<CurrencyRates>()
+        properties.map {
+            if (it is KMutableProperty<*>) {
+                val value = it.getter.call(rates)
+                val name = it.name
+                list.add(CurrencyRates(currencyName = name, value = value as Double))
+            }
+        }
+        assertEquals(list.isNotEmpty(), true)
+    }
     private fun provideApi(): ApiService = Retrofit.Builder()
         .baseUrl(Constants.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
